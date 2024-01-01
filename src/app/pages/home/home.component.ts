8993dface08f456833f59c2b1a2f16a00d7daf07
SirAnxious <SirAnxiousALot@gmail.com>
2024-01-01 14:40:59 +0200

    Add - Website Details in Home Component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { ProductsCarouselComponent } from '../../components/products-carousel/products-carousel.component';
 import { ContentCarouselComponent } from '../../components/content-carousel/content-carousel.component';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ProductsCarouselComponent, ContentCarouselComponent],
+  imports: [ProductsCarouselComponent, ContentCarouselComponent, ButtonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
