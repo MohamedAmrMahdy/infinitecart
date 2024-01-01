2e501c28633cd082702738331d81e890978d5a60
Mohamed Amr Mahdy <dev.mohamed.amr@outlook.com>
2024-01-01 13:14:39 +0200

    Clean State Mang Demo

--- src/app/stores/products.store.ts
+++ src/app/stores/products.store.ts
@@ -12,28 +12,28 @@ import {
 
 export const ProductsStore = signalStore(
   withState({ myVariable: 0 }),
-  withComputed(({ myVariable }) => ({
-    doubleMyVariable: computed(() => myVariable() * 2),
-  })),
-  withMethods(({ myVariable, ...store }) => ({
-    increment() {
-      patchState(store, { myVariable: myVariable() + 1 });
-    },
-    decrement() {
-      patchState(store, { myVariable: myVariable() - 1 });
-    },
-    reset(){
-      patchState(store, { myVariable: 0 });
-    }
-  })),
-  withHooks({
-    onInit({ increment }) {
-      interval(1_000)
-        .pipe(takeUntilDestroyed())
-        .subscribe(() => increment());
-    },
-    onDestroy({ myVariable }) {
-      console.log('count on destroy', myVariable());
-    },
-  }),
+  // withComputed(({ myVariable }) => ({
+  //   doubleMyVariable: computed(() => myVariable() * 2),
+  // })),
+  // withMethods(({ myVariable, ...store }) => ({
+  //   increment() {
+  //     patchState(store, { myVariable: myVariable() + 1 });
+  //   },
+  //   decrement() {
+  //     patchState(store, { myVariable: myVariable() - 1 });
+  //   },
+  //   reset(){
+  //     patchState(store, { myVariable: 0 });
+  //   }
+  // })),
+  // withHooks({
+  //   onInit({ increment }) {
+  //     interval(1_000)
+  //       .pipe(takeUntilDestroyed())
+  //       .subscribe(() => increment());
+  //   },
+  //   onDestroy({ myVariable }) {
+  //     console.log('count on destroy', myVariable());
+  //   },
+  // }),
 );
