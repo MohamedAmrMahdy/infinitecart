28fe28c7a7d31add48ac1813412da0ad910f7dfb
SirAnxious <SirAnxiousALot@gmail.com>
2024-01-02 23:12:10 +0200

    Add: Implement categories-carousel (currently a block)

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { ProductsCarouselComponent } from '../../components/products-carousel/products-carousel.component';
 import { ContentCarouselComponent } from '../../components/content-carousel/content-carousel.component';
 import { ButtonModule } from 'primeng/button';
+import { CategoriesCarouselComponent } from '../../components/categories-carousel/categories-carousel.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ProductsCarouselComponent, ContentCarouselComponent, ButtonModule],
+  imports: [ProductsCarouselComponent, ContentCarouselComponent, CategoriesCarouselComponent, ButtonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
