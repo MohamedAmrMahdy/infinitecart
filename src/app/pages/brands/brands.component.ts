d18b6f188ff2dad4b53d9354fb3dd2c0976c8a70
SirAnxious <SirAnxiousALot@gmail.com>
2024-01-06 23:07:43 +0200

    Add: implement Brands page

--- src/app/pages/brands/brands.component.ts
+++ src/app/pages/brands/brands.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ImageModule } from 'primeng/image';
+import { BrandsService } from '../../services/brands.service';
 
 @Component({
   selector: 'app-brands',
   standalone: true,
-  imports: [],
+  imports: [ImageModule, RouterModule],
+  providers: [BrandsService],
   templateUrl: './brands.component.html',
   styleUrl: './brands.component.css'
 })
-export class BrandsComponent {
-
+export class BrandsComponent implements OnInit {
+  brands:any;
+  constructor(private brandsService: BrandsService) { }
+  ngOnInit(): void {
+    this.brandsService.getBrands().subscribe({
+      next: (data) => {
+        this.brands = data;
+        // console.log(this.products[0]);
+      }
+    })
+  }
 }
