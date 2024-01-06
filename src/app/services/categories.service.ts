be7dbcdc6a83ddb2abfc91e8ffa901de240d1c51
SirAnxious <SirAnxiousALot@gmail.com>
2024-01-06 23:08:48 +0200

    Remove: Redundant code

--- src/app/services/categories.service.ts
+++ src/app/services/categories.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
