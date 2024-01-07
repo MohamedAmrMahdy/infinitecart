dfb6745562891c995f2472aadb106878efa009c4
Aladdin <SirAnxiousALot@gmail.com>
2024-01-07 01:41:19 +0000

    Update 5 files

--- src/app/services/brands.service.ts
+++ src/app/services/brands.service.ts
@@ -2,15 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class BrandsService {
-
-  constructor(private brands: HttpClient) { }
-  private DB_URL = 'http://localhost:3000/brands';
+  constructor(private brands: HttpClient) {}
+  private DB_URL = 'https://rstnml-3000.csb.app/brands';
 
   getBrands() {
-    return this.brands.get(this.DB_URL)
+    return this.brands.get(this.DB_URL);
   }
-
 }
